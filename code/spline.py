Bug fixed in spline coefficient computation.

code/spline.py
@@ -3,8 +3,8 @@ Defines the CubicSpline class.
 """
 
 import numpy as np
-from lu import lu
-from tls import solver
+from .lu import lu
+from .tls import solver
 
 class CubicSpline():
     """
@@ -67,13 +67,13 @@ class CubicSpline():
             np.zeros(self.size)]
         
 
-        dx = np.array([X[i + 1] - X[i] for i in range(len(X) - 1)])
-        dy = np.array([Y[i + 1] - Y[i] for i in range(len(X) - 1)])
+        dx = np.array([X[i + 1] - X[i] for i in range(self.size)])
+        dy = np.array([Y[i + 1] - Y[i] for i in range(self.size)])
         
 
         if len(X) == 2:
-            v, u, w = np.zeros(len(X) - 1), np.zeros(len(X)), np.zeros(len(X) - 1)
-            delta = np.zeros(len(X))
+            v, u, w = np.zeros(self.size), np.zeros(self.size + 1), np.zeros(self.size)
+            delta = np.zeros(self.size + 1)
 
             v[0] = 3 * dx[0]**2
             
@@ -84,23 +84,18 @@ class CubicSpline():
 
             delta[0] = dy[0] - BC[0] * dx[0]
             delta[1] = BC[1] - BC[0]
+        
         else:
-            v, u, w = np.zeros(len(X) - 2), np.zeros(len(X) - 1), np.zeros(len(X) - 2)
-            delta = np.zeros(len(X) - 1)
+            v = np.array([dx[i] for i in range(2, len(dx))])
+            w = np.array([dx[i] for i in range(len(dx) - 2)])
+            u = np.array([2 * (dx[i] + dx[i+1]) for i in range(len(dx) - 1)])
 
-            v = np.array([dx[i] for i in range(len(dx) - 1)])
-            w = np.array([dx[i] for i in range(len(dx) - 1)])
-            
-            u[0] = 2 * v[0]
-            for i in range(1, len(u) - 1, 1):
-                u[i] = 2 * (dx[i] + dx[i - 1])
-            u[-1] = 2 * dx[-1]
-
-            delta[0] = dy[0]/dx[0] - BC[0]
-            for i in range(1, len(delta) - 1, 1):
-                delta[i] = dy[i]/dx[i] - dy[i - 1]/dx[i - 1]
-            delta[-1] = BC[-1] - dy[-1]/dx[-1]
+        
+            delta = np.array([(dy[i]/dx[i] * dx[i+1] + dy[i+1]/dx[i+1] * dx[i]) for i in range(len(dx) - 1)])
             delta = 3 * delta
+
+            delta[0]  = delta[0]  - dx[1]  * BC[0]
+            delta[-1] = delta[-1] - dx[-1] * BC[-1]
 
 
         beta, alpha, gamma = lu(v, u, w)
@@ -113,16 +108,21 @@ class CubicSpline():
             self.params[2][0] = BC[0]
             self.params[3][0] = Y[0]
         else:
-            self.params[1] = sol
+            self.params[2][0] = BC[0]
+            for i in range(1, self.size):
+                self.params[2][i] = sol[i - 1]
+
+            for i in range(self.size - 1):
+                self.params[0][i] = ((self.params[2][i] + self.params[2][i + 1]) * dx[i] - 2 * dy[i]) / dx[i]**3
+            self.params[0][-1] = ((self.params[2][-1] + BC[-1]) * dx[-1] - 2 * dy[-1]) / dx[-1]**3
+
+            for i in range(self.size - 1):
+                self.params[1][i] = (3 * dy[i] - (self.params[2][i + 1] + 2 * self.params[2][i]) * dx[i]) / dx[i]**2
+            self.params[1][-1] = (3 * dy[-1] - (BC[-1] + 2 * self.params[2][-1]) * dx[-1]) / dx[-1]**2
+
             self.params[3] = np.array([Y[i] for i in range(len(X) - 1)])
             
-            self.params[2][0] = BC[0]
-            for i in range(1, len(X) - 1):
-                self.params[2][i] = self.params[2][i - 1] + dx[i - 1] * (self.params[1][i] + self.params[1][i - 1])
             
-            for i in range(len(X) - 1):
-                self.params[0][i] = (dy[i] - self.params[2][i] * dx[i] - self.params[1][i] * dx[i]**2) / dx[i]**3
-
     def eval(self, x: float) -> float:
         """
         Evaluate the spline value at a given x value.

test/lu_test.py
@@ -16,13 +16,13 @@ class TestDecompositionLU(unittest.TestCase):
     
     # test known problems
     def test_known_solution(self):
-        v, u, w = np.array([3, 2, 2]), np.array([6, 10, 8, 4]), np.array([3, 2, 2])
+        v, u, w = np.array([2, 2]), np.array([10, 8, 8]), np.array([3, 2])
         
         diags = lu(v, u, w)
         sol = [
-            np.array([3, 2, 2]), 
-            np.array([6, 17/2, 128/17, 111/32]), 
-            np.array([1/2, 4/17, 17/64])
+            np.array([2, 2]), 
+            np.array([10, 37/5, 276/37]), 
+            np.array([3/10, 10/37])
         ]
 
         for i in range(len(sol)):

test/spline_test.py
@@ -49,9 +49,9 @@ class TestCubicSpline(unittest.TestCase):
 
         cs = CubicSpline(X, Y, BC)
         sol = [
-            np.array([443/666, -153/148, 287/296, -805/296]),
-            np.array([-197/74, 123/37, -213/74, 435/148]),
-            np.array([0, 147/74, 213/74, 3]),
+            np.array([515/828, -297/368, 35/368, 203/368]),
+            np.array([-233/92, 141/46, -327/184, -111/92]),
+            np.array([0, 147/92, 96/23, -165/92]),
             np.array([2, -4, 5, 7])
         ]
 
@@ -123,6 +123,23 @@ class TestCubicSpline(unittest.TestCase):
         for i in range(len(splines)):
             for p in range(len(splines[i].params)):
                 self.assertAlmostEqual(solutions[i][p][0], splines[i].params[p][0])
+
+    def test_left_edge_derivative(self):
+        X = np.array([1, 4, 6, 8, 10])
+        Y = np.array([2, -4, 5, 7, 3])
+        BC = np.array([0, 0])
+
+        spline = CubicSpline(X, Y, BC)
+        self.assertAlmostEqual(spline.params[2][0], BC[0])
+
+    def test_right_edge_derivative(self):
+        X = np.array([1, 4, 6, 8, 10])
+        Y = np.array([2, -4, 5, 7, 3])
+        BC = np.array([0, 0])
+
+        spline = CubicSpline(X, Y, BC)
+        a, b, c, d = spline.params
+        self.assertAlmostEqual(3 * a[-1] * (X[-1] - X[-2])**2 + 2 * b[-1] * (X[-1] - X[-2]) + c[-1], BC[1])
 
 class TestEvalFunction(unittest.TestCase):
     def test_invalid_lower_input(self):

test/tls_test.py
@@ -27,8 +27,8 @@ class TestForewardSubstitution(unittest.TestCase):
 
     # check known solution
     def test_known_solution(self):
-        sol = foreward(np.array([3, 2, 2]), np.array([6, 17/2, 128/17, 111/32]), np.array([-6, 39/2, -21/2, 6]))
-        x = np.array([-1, 45/17, -537/256, 435/148])
+        sol = foreward(np.array([2, 2]), np.array([10, 37/5, 276/37]), np.array([57/2, 33, -6]))
+        x = np.array([57/20, 273/74, -165/92])
 
         for i in range(len(sol)):
             self.assertAlmostEqual(sol[i], x[i])
@@ -43,25 +43,25 @@ class TestBackwardSubstitution(unittest.TestCase):
 
     # check known solution
     def test_known_solution(self):
-        gamma = np.array([1/2, 4/17, 17/64])
-        temp = np.array([-1, 45/17, -537/256, 435/148])
+        gamma = np.array([3/10, 10/37])
+        temp = np.array([57/20, 273/74, -165/92])
 
-        b = np.array([-197/74, 123/37, -213/74, 435/148])
+        c = np.array([147/92, 96/23, -165/92])
         sol = backward(gamma, temp)
 
         for i in range(len(sol)):
-            self.assertAlmostEqual(b[i], sol[i])
+            self.assertAlmostEqual(c[i], sol[i])
 
 class TestSolver(unittest.TestCase):
     # check known solution
     def test_known_solution(self):
-        alpha = np.array([6, 17/2, 128/17, 111/32])
-        beta  = np.array([3, 2, 2])
-        gamma = np.array([1/2, 4/17, 17/64])
-        delta = np.array([-6, 39/2, -21/2, 6])
+        alpha = np.array([10, 37/5, 276/37])
+        beta  = np.array([2, 2])
+        gamma = np.array([3/10, 10/37])
+        delta = np.array([57/2, 33, -6])
 
-        b = np.array([-197/74, 123/37, -213/74, 435/148])
+        c = np.array([147/92, 96/23, -165/92])
         sol = solver(beta, alpha, gamma, delta)
 
         for i in range(len(sol)):
-            self.assertAlmostEqual(b[i], sol[i])
+            self.assertAlmostEqual(c[i], sol[i])